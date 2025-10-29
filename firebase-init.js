489acabaf3f21ff968ceccb81642cbab398d80ab
khongorzulbatchuluun61-source <khongorzulbatchuluun61@gmail.com>
2025-10-30 02:09:50 +0800

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+// Firebase тохиргоо
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+// Firebase initialize
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
